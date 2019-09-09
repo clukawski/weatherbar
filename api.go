b18c881d1166862544cc33b9b160d0b9cb40dfd0
Conrad Lukawski <conrad@backlight.ca>
2019-09-09 06:00:40 -0400

    Comment everything

--- api.go
+++ api.go
@@ -8,15 +8,21 @@ import (
 	"net/http"
 )
 
+// Various API and formula constants
 const (
 	ApiURL                = "https://api.openweathermap.org/data/2.5/weather?q=%s,%s&APPID=%s"
-	HumidexFormat         = "Outside: %s, %.1fC, feels like %.1fC"
-	WindchillFormat       = "Outside: %s, %.1fC, feels like %.1fC"
-	DefaultFormat         = "Outside: %s, %.1fC"
 	Sonntag90CoefficientA = 17.62
 	Sonntag90CoefficientB = 243.12
 )
 
+// Output formatting constants
+const (
+	HumidexFormat   = "Outside: %s, %.1fC, feels like %.1fC"
+	WindchillFormat = "Outside: %s, %.1fC, feels like %.1fC"
+	DefaultFormat   = "Outside: %s, %.1fC"
+)
+
+// Response structure of openweather API
 type WeatherResponse struct {
 	Coord struct {
 		Lon float64 `json:"lon"`
@@ -38,8 +44,8 @@ type WeatherResponse struct {
 	} `json:"main"`
 	Visibility int `json:"visibility"`
 	Wind       struct {
-		Speed float64 `json:"speed"`
-		Deg   int     `json:"deg"`
+		Speed float64 `json:"speed"` // m/s, must be converted to km/h to calculate windchill
+		Deg   float64 `json:"deg"`
 	} `json:"wind"`
 	Clouds struct {
 		All int `json:"all"`
@@ -59,6 +65,8 @@ type WeatherResponse struct {
 	Cod      int    `json:"cod"`
 }
 
+// Fetch weather data from openweather and return a pointer to
+// the unmarshaled WeatherResponse structure
 func GetWeather(apiKey, city, countryCode string) (*WeatherResponse, error) {
 	wr := new(WeatherResponse)
 
@@ -81,11 +89,13 @@ func GetWeather(apiKey, city, countryCode string) (*WeatherResponse, error) {
 	return wr, nil
 }
 
+// Convert kelvin units to celcius
 func Celcius(kelvin float64) float64 {
 	celcius := kelvin - 273.16
 	return celcius
 }
 
+// Calculates dewpoint with air temp in celcius and relative humidity
 func Dewpoint(temp float64, humidity float64) float64 {
 	alpha := func(temp float64, rh float64) float64 {
 		return math.Log(rh/100) + ((Sonntag90CoefficientA * temp) / (Sonntag90CoefficientB + temp))
@@ -96,22 +106,33 @@ func Dewpoint(temp float64, humidity float64) float64 {
 	return dewPoint
 }
 
+// Calculates windchill from air temp in celcius and wind speed in km/h
 func Windchill(temp, speed float64) float64 {
-	windChill := 13.12 + 0.6215*temp - 11.37*math.Pow(speed, 0.16) + 0.3965*math.Pow(temp, 0.16)
+	windChill := 13.12 + 0.6215*temp - 11.37*math.Pow(speed, 0.16) + 0.3965*temp*math.Pow(speed, 0.16)
 	return windChill
 }
 
+// Return a string containing basic weather information
 func (wr *WeatherResponse) String() string {
+	// Do various conversionsa
+	windSpeedKph := 3.6 * wr.Wind.Speed
 	tempCelcius := Celcius(wr.Main.Temp)
+
+	// Calculate windchill using air temp in celcius and wind speed in km/h
+	windChill := Windchill(tempCelcius, windSpeedKph)
+	// Calculate dewpoint using Sonntag90 coefficients
 	dewPointCelcius := Dewpoint(tempCelcius, float64(wr.Main.Humidity))
+	// Convert dewpoint back to kelvin for humidex calculation
 	dewPoint := dewPointCelcius + 273.15
 
+	// Calculate humidex using dew point and air temp
+	// Todo: document various equation constants
 	// Remember 5/9 is zero because 5 and 9 are integer types (do 5.0/9.0 instead)
 	e := 6.11 * math.Exp(5417.7530*((1/273.15)-(1/dewPoint)))
 	h := (5.0 / 9.0) * (e - 10)
 	humidex := wr.Main.Temp + h
-	windChill := Windchill(tempCelcius, wr.Wind.Speed)
 
+	// Determine string output based on air temperature conditions
 	var output string
 	switch true {
 	case tempCelcius < 0:
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	// Exit on incorrect number of arguments
 	if len(os.Args) < 4 {
 		os.Exit(1)
 	}
@@ -15,10 +16,13 @@ func main() {
 	city := os.Args[2]
 	countryCode := os.Args[3]
 
+	// Fetch weather data
 	wr, err := GetWeather(apiKey, city, countryCode)
 	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
+	// Display weather data
 	fmt.Println(wr.String())
 }
